79c88c479b908c04aec6a8bcdefd4894f3a795f8
Tania Nguyen <tanianguyen@Tanias-MBP.earthlink.net>
2020-09-10 13:28:58 -0500

    queries, schema, and retirement info.

--- schema.sql
+++ schema.sql
@@ -17,13 +17,13 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE salaries (
@@ -36,22 +36,20 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-  dept_no VARCHAR NOT NULL,
-  emp_no INT NOT NULL,
-  from_date DATE NOT NULL, 
-  to_date DATE NOT NULL,
-  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-  PRIMARY KEY (dept_no, emp_no)
+    emp_no INT NOT NULL,
+    dept_no VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
+
 
 CREATE TABLE titles (
   emp_no INT NOT NULL,
   title VARCHAR NOT NULL, 
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
