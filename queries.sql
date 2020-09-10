9597c2f234796ad4d6a63ad2727273b7039fe3a0
Tania Nguyen <tanianguyen@Tanias-MBP.earthlink.net>
2020-09-10 15:20:11 -0500

    new csv.

--- queries.sql
+++ queries.sql
@@ -25,3 +25,70 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- joinging with nicknames departments and dept_manager
+SELECT d.dept_name,
+    dm.emp_no,
+    dm.from_date,
+    dm.to_date
+FROM departments as d 
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Create new table with emp_no column for retirement info
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+    retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+	
+
+-- creating joins with nicknames
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+-- joining retirement info and dept_emp
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+    de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO count_dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
