d5fde6f6f3bc124f9f70862e62300e3a64103d99
Tania Nguyen <tanianguyen@Tanias-MBP.earthlink.net>
2020-09-11 14:17:24 -0500

    completed deliverable 1.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,11 +1,10 @@
--- Creating table for number of retiring employees by title
 SELECT e.emp_no, 
     e.first_name,
     e.last_name,
     t.title,
     t.from_date,
     t.to_date
--- INTO retiring_employees
+INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as t
 ON e.emp_no = t.emp_no
@@ -13,11 +12,18 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+    rt.first_name,
+    rt.last_name,
+    rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.title DESC;
 
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
+
+-- number of employees by their most recent job title who are about to retire.
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title 
+ORDER BY COUNT (ut.title) DESC;
