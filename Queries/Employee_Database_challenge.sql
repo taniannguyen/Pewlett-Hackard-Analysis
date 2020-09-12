210ee7e4b3415507cf77700503cf825fa91a23b9
Tania Nguyen <tanianguyen@Tanias-MBP.earthlink.net>
2020-09-12 10:32:15 -0500

    new csv and completed sql doc.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -12,7 +12,7 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+SELECT DISTINCT ON (emp_no) rt.emp_no,
     rt.first_name,
     rt.last_name,
     rt.title
@@ -29,19 +29,20 @@ GROUP BY ut.title
 ORDER BY COUNT (ut.title) DESC;
 
 
+
 -- Mentorship Eligibility employees who are eligible mentorship program
-SELECT DISTINCT ON (e.emp_no) e.emp_no,
+SELECT DISTINCT ON (emp_no) e.emp_no,
     e.first_name,
     e.last_name,
     e.birth_date,
     de.from_date,
     de.to_date,
     t.title
--- INTO mentorship_eligibility
-FROM employees as e
-INNER JOIN dept_emp as de 
-ON (e.emp_no = de.emp_no)
+INTO mentorship_eligibility
+FROM dept_emp as de
+INNER JOIN employees as e
+ON (de.emp_no = e.emp_no)
 INNER JOIN titles as t 
 ON (e.emp_no = t.emp_no)
-WHERE (de.to_date BETWEEN '1965-01-01' AND '1965-12-31')
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
